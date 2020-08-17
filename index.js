4acbb6f480e7f82e76c2dd58805b2a162edb8dea
WeHaveJoy <cnovuyo10@gmail.com>
2020-08-18 00:37:35 +0200

    new changes

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const settingsBill = require('./settings-bill');
+
+const app = express();
+const settingsBill = SettingsBill();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        settings: settingsBill.getSettings()
+        totals: settingsBill.totals()
+
+    });
+});
+
+app.post('/settings', function (req, res) {
+
+    settingsBill.setSettings({
+        callCost: req.body.callCost,
+        smsCost: req.body.smsCost,
+        warningLevel: req.body.warningLevel,
+        criticalLevel: req.body.criticalLevel
+    });
+    // Console.log(settingsBill.getSettings());
+    res.redirect('/');
+});
+
+
+app.post('/action', function (req, res) {
+    settingsBill.recordAction(req.body.actionType)
+
+    res.redirect('/');
+});
+
+app.get('/actions', function (req, res) {
+    res.render('actions', { actions: settingsBill.actions() });
+});
+
+app.get('/action/:type', function (req, res) {
+
+});
+
+const PORT = process.env.PORT || 3011
+app.listen(PORT, function () {
+    Console.log("App started at PORT:", PORT)
+});
